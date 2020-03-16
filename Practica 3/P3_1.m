7a8fd2faeb8e52e6d64af283ee4d938dc979e15d
Uriel Soubran <uriel_bee15@hotmail.com>
2020-03-16 15:19:04 -0600

    Practica 3
    Contiene las imágenes descargadas de LAPI dentro de la carpeta Imagenes.
    
    Además, contiene dos archivos:
    P3_1.m
    P3_2.m
    _X para el ejercicio X, incluyendo sub incisos.
    
    El ejercicio 2 aun no se termina.

---
+++ Practica 3/P3_1.m
@@ -0,0 +1,48 @@
+clear variables; close all; clc;
+
+% Seleccionar una imagen tipo tif o tiff utilizando el explorador de
+% archivos.
+[filename, filepath] = uigetfile('*.tif*', 'Select a tif file');
+fullname = fullfile(filepath, filename);
+TheImage = imread(fullname);
+
+% Histograma, ecualizacion e histograma de la ecualizacion.
+imageHist = imhist(TheImage);
+imageEqu = histeq(TheImage);
+histEqu = imhist(imageEqu);
+
+% Incisos A y B
+figure(1);
+subplot(2,2,1);
+imshow(TheImage);
+title('Imagen Original');
+subplot(2,2,2);
+plot(imageHist);
+title('Histograma Original');
+subplot(2,2,3);
+imshow(imageEqu);
+title('Ecualización de la Imagen');
+subplot(2,2,4);
+plot(histEqu);
+title('Histograma de la imagen ecualizada');
+
+% Inciso C
+niveles = 128;
+while(1);
+    histo = imhist(TheImage,niveles);
+    ecual = histeq(TheImage,histo);
+    histEcual = imhist(ecual);
+    figure(2);
+    subplot(1,2,1);
+    imshow(ecual);
+    title(strcat('Ecualizacion a',' ',int2str(niveles),' niveles'));
+    subplot(1,2,2);
+    plot(histEcual);
+    title('Histograma');
+    pause(2);
+    if(niveles ~= 2)
+        niveles = niveles/2;
+    else
+        niveles = 128;
+    end
+end
