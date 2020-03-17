f577ef66def728673051aa51372a775292939865
Uriel Soubran <uriel_bee15@hotmail.com>
2020-03-16 19:07:47 -0600

    Ejercicio 2 - b
    Solo están concatenados. Creo que sirve como quieren (?)

--- Practica 3/P3_2.m
+++ Practica 3/P3_2.m
@@ -20,9 +20,9 @@ imshow(Satelite2);
 title('Imagen Satelital 2 Original');
 
 % Inciso A. Recorte de imagen sin el área negra.
-figure();
-recorteA = imcrop(Satelite1);
-recorteB = imcrop(Satelite2);
+% figure();
+recorteA = imcrop(Satelite1,[0 0 1761 1500]);
+recorteB = imcrop(Satelite2,[1229.5 0.5 1271 1500]);
 histA = imhist(recorteA);
 histB = imhist(recorteB);
 figure();
@@ -41,3 +41,10 @@ subplot(1,2,2);
 plot(histB);
 title('Histograma 2');
     
+% Inciso B. Imagen unida sin especificacion del histograma.
+union = imfuse(recorteA,recorteB,'montage');
+figure();
+imshow(union);
+title('Imagenes unidas sin especificacion de histograma');
+% Con montage, se "concatenan" dos imágenes, pero como no son del mismo
+% tamaño, añade ceros a la matriz para dejarla del mismo tamaño.
