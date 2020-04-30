e8937586fde4e5a5d066d3bc6909ccb79f3be96f
Zelpo <diego_omar5@hotmail.com>
2020-04-30 18:31:52 -0500

    Por si algun dia lo necesitan, incluye los filtros

--- Practica 5/practica5bis.m
+++ Practica 5/practica5bis.m
@@ -54,8 +54,32 @@ title('DFT de sobremuestreo con T = 4');
 % Factor de escala T = 2
 T2 = 0.5;
 [p, q] = meshgrid(1:T2:n, 1:T2:m);
+
 % Orden 0
 interpolOrd0T2 = interp2(x, y, pentagono, p, q, 'nearest');
+% Orden 1
+interpolOrd1T2 = interp2(x, y, pentagono, p, q, 'linear');
+% Orden 3
+interpolOrd3T2 = interp2(x, y, pentagono, p, q, 'cubic');
+
+% Filtros para T = 2
+filtro0T2 = ones(2, 2);
+interpolOrd0T2 = conv2(sobremuesCon2, filtro0T2);
+filtro1T2 = ([0.5, 1, 0.5].*[0.5; 1; 0.5]);
+interpolOrd1T2 = conv2(sobremuesCon2, filtro1T2);
+filtro3T2 = [];
+for factor = abs(-2:0.5:2)
+    if (factor >= 0) && (factor < 1)
+        filtro3T2 = [filtro3T2 (((3/2) * power(factor, 3))-((5/2) * power(factor, 2))+1)];
+    else
+        if (factor >= 1) && (factor < 2)
+            filtro3T2 = [filtro3T2 (((-1/2) * power(factor, 3))+((5/2) * power(factor, 2))-(4 * factor)+2)];
+        end
+    end
+end
+filtro3T2 = filtro3T2'.*filtro3T2;
+interpolOrd3T2 = conv2(sobremuesCon2, filtro3T2);
+
 figure(3);
 subplot(1, 3, 1);
 imagesc(interpolOrd0T2);
@@ -63,8 +87,6 @@ axis image;
 colormap('gray');
 zoom(3);
 title('Interpolación orden 0 con T = 2');
-% Orden 1
-interpolOrd1T2 = interp2(x, y, pentagono, p, q, 'linear');
 figure(3);
 subplot(1, 3, 2);
 imagesc(interpolOrd1T2);
@@ -72,8 +94,6 @@ axis image;
 colormap('gray');
 zoom(3);
 title('Interpolación orden 1 con T = 2');
-% Orden 3
-interpolOrd3T2 = interp2(x, y, pentagono, p, q, 'cubic');
 figure(3);
 subplot(1, 3, 3);
 imagesc(interpolOrd3T2);
@@ -85,8 +105,32 @@ title('Interpolación orden 3 con T = 2');
 % Factor de escala T = 4
 T4 = 0.25;
 [p, q] = meshgrid(1:T4:n, 1:T4:m);
+
 % Orden 0
 interpolOrd0T4 = interp2(x, y, pentagono, p, q, 'nearest');
+% Orden 1
+interpolOrd1T4 = interp2(x, y, pentagono, p, q, 'linear');
+% Orden 3
+interpolOrd3T4 = interp2(x, y, pentagono, p, q, 'cubic');
+
+% Filtros para T = 4
+filtro0T4 = ones(4, 4);
+interpolOrd0T4 = conv2(sobremuesCon4, filtro0T4);
+filtro1T4 = ([0.25, 0.5, 0.75, 1, 0.75, 0.5, 0.25].*[0.25; 0.5; 0.75; 1; 0.75; 0.5; 0.25]);
+interpolOrd1T4 = conv2(sobremuesCon4, filtro1T4);
+filtro3T4 = [];
+for factor = abs(-2:0.25:2)
+    if (factor >= 0) && (factor < 1)
+        filtro3T4 = [filtro3T4 (((3/2) * power(factor, 3))-((5/2) * power(factor, 2))+1)];
+    else
+        if (factor >= 1) && (factor < 2)
+            filtro3T4 = [filtro3T4 (((-1/2) * power(factor, 3))+((5/2) * power(factor, 2))-(4 * factor)+2)];
+        end
+    end
+end
+filtro3T4 = filtro3T4'.*filtro3T4;
+interpolOrd3T4 = conv2(sobremuesCon4, filtro3T4);
+
 figure(4);
 subplot(1, 3, 1);
 imagesc(interpolOrd0T4);
@@ -94,8 +138,6 @@ axis image;
 colormap('gray');
 zoom(3);
 title('Interpolación orden 0 con T = 4');
-% Orden 1
-interpolOrd1T4 = interp2(x, y, pentagono, p, q, 'linear');
 figure(4);
 subplot(1, 3, 2);
 imagesc(interpolOrd1T4);
@@ -103,8 +145,6 @@ axis image;
 colormap('gray');
 zoom(3);
 title('Interpolación orden 1 con T = 4');
-% Orden 3
-interpolOrd3T4 = interp2(x, y, pentagono, p, q, 'cubic');
 figure(4);
 subplot(1, 3, 3);
 imagesc(interpolOrd3T4);
@@ -196,6 +236,51 @@ axis image;
 colormap('gray');
 title('Inv del DFT con ceros a 512');
 subplot(1, 2, 2);
+imagesc(originaldftInv1024);
+axis image;
+colormap('gray');
+title('Inv del DFT con ceros a 1024');
+
+%% SecciÃ³n del cuarto ejercicio de la prÃ¡ctica 5
+figure(9);
+subplot(2, 2, 1);
+imagesc(interpolOrd0T2);
+axis image;
+colormap('gray');
+title('Interpolación orden 0 con T = 2');
+subplot(2, 2, 2);
+imagesc(interpolOrd1T2);
+axis image;
+colormap('gray');
+title('Interpolación orden 1 con T = 2');
+subplot(2, 2, 3);
+imagesc(interpolOrd3T2);
+axis image;
+colormap('gray');
+title('Interpolación orden 3 con T = 2');
+subplot(2, 2, 4);
+imagesc(originaldftInv512);
+axis image;
+colormap('gray');
+title('Inv del DFT con ceros a 512');
+
+figure(10);
+subplot(2, 2, 1);
+imagesc(interpolOrd0T4);
+axis image;
+colormap('gray');
+title('Interpolación orden 0 con T = 4');
+subplot(2, 2, 2);
+imagesc(interpolOrd1T4);
+axis image;
+colormap('gray');
+title('Interpolación orden 1 con T = 4');
+subplot(2, 2, 3);
+imagesc(interpolOrd3T4);
+axis image;
+colormap('gray');
+title('Interpolación orden 3 con T = 4');
+subplot(2, 2, 4);
 imagesc(originaldftInv1024);
 axis image;
 colormap('gray');
