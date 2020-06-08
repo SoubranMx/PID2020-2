e055137598c875df37463435029fee8a55cb2f9a
Zelpo <diego_omar5@hotmail.com>
2020-06-07 20:11:37 -0500

    Hace tres segmentaciones

--- final/proyecto.m
+++ final/proyecto.m
@@ -2,46 +2,89 @@
 clear variables; close all; clc;
 
 % Lectura de las imagenes
+%imagen = mat2gray(niftiread('tr_im.nii.gz'));
 imagen = mat2gray(niftiread('val_im.nii.gz'));
 
-% Despliegue de las imagenes
+% Despliegue de las imagenes completas
 for i = (1:10)
     figure(i);
-    subplot(1, 2, 1);
+    subplot(2, 2, 1);
     imagesc(imagen(:, :, i));
     axis image;
     colormap('gray');
 end
 
-% Primera segmentacion, los pulmones
+%% Primera segmentacion
 for j = (1:10)
     
     % Se filtra la imagen
-    filtroBloque = ones(11, 11)*(1/121);
+    filtroBloque = ones(11, 11)*(2/120);
     imagenFiltrada = conv2(imagen(:, :, j), filtroBloque, 'same');
 
-%     % Histograma de la imagen
+    % Histograma de la imagen
 %     figure;
 %     imhist(imagenFiltrada);
     [counts, x] = imhist(imagenFiltrada);
 
-    % Obtencion de una máscara con otsuthresh
+    % Obtencion de una mascara con otsuthresh
     T = otsuthresh(counts);
     BW = imbinarize(imagenFiltrada, T);
-%     figure;
-%     imshow(BW);
 
-    % Inversion de los valores de la mascara
-    masca = BW + 1;
-    masca(masca==2)=0;
+    % Asignar la máscara a una variable
+    masca = BW ;
+%     figure;
+%     imshow(masca);
 
     % Segmentacion por multiplicacion con la mascara
     segmento1 = masca .* imagen(:, :, j);
 
-    % Despliegue de los pulmones segmentados
+    % Despliegue de la primera segmentacion
     figure(j);
-    subplot(1, 2, 2);
+    subplot(2, 2, 2);
     imagesc(segmento1);
     axis image;
     colormap('gray');
+    
+%% Segunda segmentacion
+    
+    % Se filtra la imagen
+    filtroBloque2 = ones(11, 11)*(1/60);
+    imagenFiltrada2 = conv2(imagen(:, :, j), filtroBloque2, 'same');
+
+    % Histograma de la imagen
+%     figure;
+%     imhist(imagenFiltrada);
+    [counts2, x2] = imhist(imagenFiltrada2);
+
+    % Obtencion de una mascara con otsuthresh
+    T2 = 1;
+    BW2 = imbinarize(imagenFiltrada2, T2);
+
+    % Inversion de los valores de la mascara
+    masca2 = BW2 +1 ;
+    masca2(masca2==2)=0;
+%     figure;
+%     imshow(masca2);
+
+    % Segmentacion por multiplicacion con la mascara
+    segmento12 = masca2 .* imagen(:, :, j);
+
+    % Despliegue de los pulmones segmentados
+    figure(j);
+    subplot(2, 2, 3);
+    imagesc(segmento12);
+    axis image;
+    colormap('gray');
+    
+    % Se obtiene la máscara para el vidrio despulido y se segmenta
+    mascaFinal = masca .* masca2;
+    segmento123 = mascaFinal .* imagen(:, :, j);
+    
+    % Despliegue del resultado final
+    figure(j);
+    subplot(2, 2, 4);
+    imagesc(segmento123);
+    axis image;
+    colormap('gray');
+    
 end
